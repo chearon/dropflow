ea1035fa17d1e853e9903190e8a765102b5fccb1
Caleb Hearon <caleb@chearon.net>
2022-09-12 19:32:49 -0400

    update ts output

--- io/node.js
+++ io/node.js
@@ -4,6 +4,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.getBuffer = void 0;
 const promises_1 = require("fs/promises");
 async function getBuffer(path) {
-    return await promises_1.readFile(path);
+    return await (0, promises_1.readFile)(path);
 }
 exports.getBuffer = getBuffer;

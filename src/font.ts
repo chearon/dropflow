58f75b360c18f32f55994ada3fe70534d803e881
Caleb Hearon <caleb@chearon.net>
2023-05-17 21:46:40 -0400

    fonts: remove 'cache' from map names
    they aren't caches

--- src/font.ts
+++ src/font.ts
@@ -6,17 +6,19 @@ import type {HbFace} from 'harfbuzzjs';
 import type {Style} from './cascade.js';
 import type {Cascade} from 'fontconfig';
 
-const fontBufferCache = new Map<string, ArrayBuffer>();
+const fontBuffers = new Map<string, ArrayBuffer>();
 
 export function registerFont(buffer: Uint8Array, filename: string) {
-  if (!fontBufferCache.has(filename)) {
+  if (!fontBuffers.has(filename)) {
     fcfg.addFont(buffer, filename);
-    fontBufferCache.set(filename, buffer);
+    fontBuffers.set(filename, buffer);
   }
 }
 
+// TODO deregisterFont
+
 function getFontBuffer(filename: string) {
-  const buffer = fontBufferCache.get(filename);
+  const buffer = fontBuffers.get(filename);
   if (!buffer) throw new Error(`${filename} not found`);
   return buffer;
 }
@@ -26,18 +28,16 @@ function createFace(filename: string, index: number) {
   const blob = hb.createBlob(buffer);
   const face = hb.createFace(blob, index);
   face.name = basename(filename); // TODO can it be done in hbjs?
-  // TODO: right now I'm not ever freeing blobs or faces. this is okay for most
-  // usages, but I should implement an LRU or something
   return face;
 }
 
-const hbFaceCache = new Map<string, HbFace>();
+const hbFaces = new Map<string, HbFace>();
 
 export function getFace(filename: string, index: number) {
-  let face = hbFaceCache.get(filename + index);
+  let face = hbFaces.get(filename + index);
   if (!face) {
     face = createFace(filename, index);
-    hbFaceCache.set(filename + index, face);
+    hbFaces.set(filename + index, face);
   }
   return face;
 }
@@ -66,11 +66,11 @@ export function createFontKey(s: Style, script: string) {
   return hash;
 }
 
-const cascadeCache = new Map<number, Cascade>();
+const cascades = new Map<number, Cascade>();
 
 export function getCascade(style: Style, script: string) {
   const fontKey = createFontKey(style, script);
-  let cascade = cascadeCache.get(fontKey);
+  let cascade = cascades.get(fontKey);
   if (!cascade) {
     const family = style.fontFamily;
     const weight = String(style.fontWeight);
@@ -78,7 +78,7 @@ export function getCascade(style: Style, script: string) {
     const slant = style.fontStyle;
     const lang = langForScript(script);
     cascade = fcfg.sort({family, weight, width, slant, lang});
-    cascadeCache.set(fontKey, cascade);
+    cascades.set(fontKey, cascade);
   }
   return cascade;
 }

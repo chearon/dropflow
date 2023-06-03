9e2b43d343055b4386f829298e18032c39dbcede
Caleb Hearon <caleb@chearon.net>
2023-06-03 18:07:03 -0400

    add tiny layout perf example
    currently hovers at 75-80µs. I'd love to get it to 50µs, but the
    profiler would only show me just under 4 samples inside of 50µs...

---
+++ examples/perf-3.ts
@@ -0,0 +1,28 @@
+import * as oflo from '../src/api.js';
+import {registerFontAsset} from '../assets/register.js';
+import {bench, run} from 'mitata';
+
+console.time('Add fonts');
+registerFontAsset('Roboto/Roboto-Regular.ttf');
+console.timeEnd('Add fonts');
+console.log();
+
+function word() {
+  let ret = '';
+  for (let i = 0; i < 10; i++) {
+    ret += String.fromCharCode(97 + Math.floor(Math.random() * 26));
+  }
+  return ret;
+}
+
+const words: string[] = [];
+
+for (let i = 0; i < 10000; i++) words.push(word());
+
+bench('generate and layout one random word', () => {
+  const rootElement = oflo.dom(words[Math.floor(Math.random() * words.length)]);
+  const blockContainer = oflo.generate(rootElement);
+  oflo.layout(blockContainer, 100, 20);
+});
+
+await run();

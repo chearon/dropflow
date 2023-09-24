d55c07a458d5ec507d04c22d6124303fecd13fa6
Caleb Hearon <caleb@chearon.net>
2023-09-24 16:28:56 -0400

    don't register fonts with node-canvas twice
    until I refactor that api this causes problems that for some
    reason only visible in bun right now

--- src/register-paint-font-node.ts
+++ src/register-paint-font-node.ts
@@ -1,13 +1,16 @@
 import type {FaceMatch} from './font.js';
 
+const alreadyRegistered = new Set<string>();
+
 try {
   var canvas = await import('canvas');
 } catch (e) {
 }
 
 export default function registerPaintFont(match: FaceMatch, buffer: Uint8Array, filename: string) {
-  if (canvas?.registerFont) {
+  if (canvas?.registerFont && !alreadyRegistered.has(filename)) {
     const descriptor = match.toCssDescriptor();
     canvas.registerFont(filename, descriptor);
+    alreadyRegistered.add(filename);
   }
 }

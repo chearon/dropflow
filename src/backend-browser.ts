09580dc95e3f02d6c2ad1028630a5398b19fe11d
Caleb Hearon <caleb@chearon.net>
2023-11-06 22:23:20 -0500

    node 20 types

--- src/backend-browser.ts
+++ src/backend-browser.ts
@@ -1,8 +1,12 @@
 import type {FaceMatch} from './font.js';
 
+// TypeScript does not support different possibilities of runtime environments,
+// so the types loaded are for node. To add the browser environment too would
+// add too many globals.
+//
+// https://gist.github.com/RyanCavanaugh/702ebd1ca2fc060e58e634b4e30c1c1c
 declare const document: any;
 declare const FontFace: any;
-declare const fetch: any;
 
 export function registerPaintFont(match: FaceMatch, buffer: Uint8Array, filename: string) {
   const descriptor = match.toCssDescriptor();

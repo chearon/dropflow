02e4cb44cb440f9e5d0ac384cad3358cc308dbcd
Caleb Hearon <caleb@chearon.net>
2025-06-11 20:47:09 +0100

    change api tests to use flow import

--- test/api.spec.js
+++ test/api.spec.js
@@ -2,13 +2,13 @@
 import {expect} from 'chai';
 import {registerFontAsset, unregisterFontAsset} from '../assets/register.js';
 import {HTMLElement} from '../src/dom.js';
-import {h, dom, generate, layout, style} from 'dropflow';
+import * as flow from 'dropflow';
 import parse from 'dropflow/parse.js';
 
 describe('Hyperscript API', function () {
   describe('h()', function () {
     it('accepts children argument', function () {
-      const div = h('div', [h('span'), 'chocolate']);
+      const div = flow.h('div', [flow.h('span'), 'chocolate']);
       expect(div.tagName).to.equal('div');
       expect(div.children).to.have.lengthOf(2);
       expect(div.children[0].tagName).to.equal('span');
@@ -16,15 +16,15 @@ describe('Hyperscript API', function () {
     });
 
     it('accepts text content argument', function () {
-      const div = h('div', 'text content');
+      const div = flow.h('div', 'text content');
       expect(div.tagName).to.equal('div');
       expect(div.children).to.have.lengthOf(1);
       expect(div.children[0].text).to.equal('text content');
     });
 
     it('accepts attrs argument', function () {
-      const div = h('div', {
-        style: style({fontSize: 66}),
+      const div = flow.h('div', {
+        style: flow.style({fontSize: 66}),
         attrs: {direction: 'rtl'}
       });
       expect(div.declaredStyle.properties.fontSize).to.equal(66);
@@ -32,14 +32,14 @@ describe('Hyperscript API', function () {
     });
 
     it('accepts attrs, children arguments', function () {
-      const div = h('div', {style: style({lineHeight: 20})}, [h('span')]);
+      const div = flow.h('div', {style: flow.style({lineHeight: 20})}, [flow.h('span')]);
       expect(div.declaredStyle.properties.lineHeight).to.equal(20);
       expect(div.children).to.have.lengthOf(1);
       expect(div.children[0].tagName).to.equal('span');
     });
 
     it('accepts attrs, text content arguments', function () {
-      const div = h('div', {style: style({display: 'inline'})}, 'text content');
+      const div = flow.h('div', {style: flow.style({display: 'inline'})}, 'text content');
       expect(div.declaredStyle.properties.display).to.equal('inline');
       expect(div.children).to.have.lengthOf(1);
       expect(div.children[0].text).to.equal('text content');
@@ -48,39 +48,43 @@ describe('Hyperscript API', function () {
 
   describe('dom()', function () {
     it('sets parents', function () {
-      const div = dom(h('div', [h('div')]));
+      const div = flow.dom(flow.h('div', [flow.h('div')]));
       expect(div.children[0].parent).to.equal(div);
       expect(div.children[0].children[0].parent).to.equal(div.children[0]);
     });
 
     it('computes styles', function () {
-      const h1 = dom(h('div', {style: style({fontSize: 99})}, 'abc'));
+      const style1 = flow.style({fontSize: 99});
+      const h1 = flow.dom(flow.h('div', {style: style1}, 'abc'));
       expect(h1.children[0].style.fontSize).to.equal(99);
-      const h2 = dom(h('div', {style: style({lineHeight: {value: 123, unit: null}})}, [h('div')]));
+
+      const style2 = flow.style({lineHeight: {value: 123, unit: null}});
+      const h2 = flow.dom(flow.h('div', {style: style2}, [flow.h('div')]));
       expect(h2.children[0].style.lineHeight).to.deep.equal(123 * 16);
     });
 
     it('cascades styles', function () {
-      const s1 = style({textAlign: 'left', fontWeight: 900});
-      const s2 = style({textAlign: 'right'});
-      const e = dom(h('div', {style: [s2, s1]}));
+      const s1 = flow.style({textAlign: 'left', fontWeight: 900});
+      const s2 = flow.style({textAlign: 'right'});
+      const e = flow.dom(flow.h('div', {style: [s2, s1]}));
       expect(e.children[0].style.textAlign).to.equal('right');
       expect(e.children[0].style.fontWeight).to.equal(900);
     });
 
     it('uses the html element', function () {
-      expect(dom(h('html', {})).children.length).to.equal(0);
+      expect(flow.dom(flow.h('html', {})).children.length).to.equal(0);
     });
 
     it('makes the root element block-level, flow-root', function () {
-      const html = dom(h('html', {style: style({display: {outer: 'inline', inner: 'flow'}})}));
+      const style = flow.style({display: {outer: 'inline', inner: 'flow'}});
+      const html = flow.dom(flow.h('html', {style: style}));
       expect(html.style.display.outer).to.equal('block');
       expect(html.style.display.inner).to.equal('flow-root');
     });
 
     it('accepts a mixed list', function () {
-      const html = dom([
-        h('div', {attrs: {div1: 'div1'}}),
+      const html = flow.dom([
+        flow.h('div', {attrs: {div1: 'div1'}}),
         'text1'
       ]);
 
@@ -91,16 +95,21 @@ describe('Hyperscript API', function () {
   });
 
   it('lays out successfully', async function () {
-    const tree = dom([
-      h('div', {style: style({fontSize: 10, lineHeight: 20, fontFamily: ['Arimo']})}, [
-        h('div', 'Chapter 1'),
-        h('div', {attrs: {id: 't'}}, ['The quick brown fox jumps over the lazy dog', h('br'), 'The end'])
+    const style = flow.style({fontSize: 10, lineHeight: 20, fontFamily: ['Arimo']});
+    const tree = flow.dom([
+      flow.h('div', {style}, [
+        flow.h('div', 'Chapter 1'),
+        flow.h('div', {attrs: {id: 't'}}, [
+          'The quick brown fox jumps over the lazy dog',
+          flow.h('br'),
+          'The end'
+        ])
       ])
     ]);
 
-    const box = generate(tree);
+    const box = flow.generate(tree);
     registerFontAsset('Arimo/Arimo-Regular.ttf');
-    layout(box, 100);
+    flow.layout(box, 100);
     const ifc = box.children[0].children[1].children[0];
     expect(ifc.paragraph.lineboxes).to.have.lengthOf(4);
     expect(ifc.paragraph.lineboxes[0].blockOffset).to.equal(0);
@@ -115,17 +124,22 @@ describe('Hyperscript API', function () {
   });
 
   it('layout twice is successful', async function () {
-    const tree = dom([
-      h('div', {style: style({fontSize: 10, lineHeight: 20, fontFamily: ['Arimo']})}, [
-        h('div', 'Chapter 1'),
-        h('div', {attrs: {id: 't'}}, ['The quick brown fox jumps over the lazy dog', h('br'), 'The end'])
+    const style = flow.style({fontSize: 10, lineHeight: 20, fontFamily: ['Arimo']});
+    const tree = flow.dom([
+      flow.h('div', {style}, [
+        flow.h('div', 'Chapter 1'),
+        flow.h('div', {attrs: {id: 't'}}, [
+          'The quick brown fox jumps over the lazy dog',
+          flow.h('br'),
+          'The end'
+        ])
       ])
     ]);
 
-    const box = generate(tree);
+    const box = flow.generate(tree);
     registerFontAsset('Arimo/Arimo-Regular.ttf');
-    layout(box, 100);
-    layout(box, 100);
+    flow.layout(box, 100);
+    flow.layout(box, 100);
     const ifc = box.children[0].children[1].children[0];
     expect(ifc.paragraph.lineboxes).to.have.lengthOf(4);
     unregisterFontAsset('Arimo/Arimo-Regular.ttf');
@@ -189,7 +203,7 @@ describe('DOM API', function () {
 
   it('exposes boxes', function () {
     const html = parse('<div id="d" style="width: 100px; height: 100px;"></div>');
-    layout(generate(html));
+    flow.layout(flow.generate(html));
     const [box] = html.query('#d').boxes;
     expect(box.getContentArea().width).to.equal(100);
     expect(box.getContentArea().height).to.equal(100);

chearon/dropflow

commit c440e11e987384dc39aa9a588b285e0ca90a61aa

move recursing functions into top-level functions
- A this parameter can't be a union type
- Generally, methods when only the object itself is needed to express
  something, functions otherwise (such as when tree traversal is needed)
- I want to move to concrete (union) types instead of base classes to avoid
  type erasure. That's coming up next. It'll help me keep an eye on what
  affects the poly/megamorphic ICs, is easier to port to a compiled language,
  and less likely to result in programmer error since you'll naturally want to
  exhaust all types (though I see no good way to enforce this with TS).

src/layout-flow.ts
@@ -747,40 +747,6 @@ export class BlockContainer extends FormattingBox {
     this.children = children;
   }
 
-  contribution(mode: 'min-content' | 'max-content'): number {
-    const marginLineLeft = this.style.getMarginLineLeft(this);
-    const marginLineRight = this.style.getMarginLineRight(this);
-    const borderLineLeftWidth = this.style.getBorderLineLeftWidth(this);
-    const paddingLineLeft = this.style.getPaddingLineLeft(this);
-    const paddingLineRight = this.style.getPaddingLineRight(this);
-    const borderLineRightWidth = this.style.getBorderLineRightWidth(this);
-    let isize = this.style.getInlineSize(this);
-    let contribution = (marginLineLeft === 'auto' ? 0 : marginLineLeft)
-      + borderLineLeftWidth
-      + paddingLineLeft
-      + paddingLineRight
-      + borderLineRightWidth
-      + (marginLineRight === 'auto' ? 0 : marginLineRight);
-
-    if (isize === 'auto') {
-      isize = 0;
-      if (this.isBlockContainerOfBlocks()) {
-        for (const child of this.children) {
-          isize = Math.max(isize, child.contribution(mode));
-        }
-      } else if (this.isBlockContainerOfInlines()) {
-        const [ifc] = this.children;
-        if (ifc.shouldLayoutContent()) {
-          isize = ifc.paragraph.contribution(mode);
-        }
-      }
-    }
-
-    contribution += isize;
-
-    return contribution;
-  }
-
   getLogSymbol() {
     if (this.isFloat()) {
       return '○︎';
@@ -813,35 +779,6 @@ export class BlockContainer extends FormattingBox {
     const lineRight = inlineSize - lineLeft - cInlineSize;
 
     return {blockStart, lineLeft, lineRight};
-  }
-
-  getLastBaseline(): number | undefined {
-    const stack: {block: BlockContainer, offset: number}[] = [{block: this, offset: 0}];
-
-    while (stack.length) {
-      const {block, offset} = stack.pop()!;
-
-      if (block.isBlockContainerOfInlines()) {
-        const [ifc] = block.children;
-        const linebox = ifc.paragraph.lineboxes.at(-1);
-        if (linebox) return offset + linebox.blockOffset + linebox.ascender;
-      }
-
-      if (block.isBlockContainerOfBlocks()) {
-        const parentOffset = offset;
-
-        for (const child of block.children) {
-          if (child.isBlockContainer()) {
-            const offset = parentOffset
-              + child.getBorderArea().blockStart
-              + child.style.getBorderBlockStartWidth(child);
-              + child.style.getPaddingBlockStart(child);
-
-            stack.push({block: child, offset});
-          }
-        }
-      }
-    }
   }
 
   isBlockContainer(): this is BlockContainer {
@@ -1063,6 +1000,47 @@ function doBlockBoxModelForFloatBox(box: FormattingBox) {
   if (size !== undefined) box.setBlockSize(size);
 }
 
+export function layoutContribution(
+  box: BlockLevel,
+  mode: 'min-content' | 'max-content'
+) {
+  const marginLineLeft = box.style.getMarginLineLeft(box);
+  const marginLineRight = box.style.getMarginLineLeft(box);
+  const borderLineLeftWidth = box.style.getBorderLineLeftWidth(box);
+  const paddingLineLeft = box.style.getPaddingLineLeft(box);
+  const paddingLineRight = box.style.getPaddingLineRight(box);
+  const borderLineRightWidth = box.style.getBorderLineRightWidth(box);
+  let isize = box.style.getInlineSize(box);
+  let contribution = (marginLineLeft === 'auto' ? 0 : marginLineLeft)
+    + borderLineLeftWidth
+    + paddingLineLeft
+    + paddingLineRight
+    + borderLineRightWidth
+    + (marginLineRight === 'auto' ? 0 : marginLineRight);
+
+  if (isize === 'auto') {
+    if (box.isReplacedBox()) {
+      isize = box.getIntrinsicIsize();
+    } else {
+      isize = 0;
+      if (box.isBlockContainerOfBlocks()) {
+        for (const child of box.children) {
+          isize = Math.max(isize, layoutContribution(child, mode));
+        }
+      } else if (box.isBlockContainerOfInlines()) {
+        const [ifc] = box.children;
+        if (ifc.shouldLayoutContent()) {
+          isize = ifc.paragraph.contribution(mode);
+        }
+      }
+    }
+  }
+
+  contribution += isize;
+
+  return contribution;
+}
+
 export function layoutFloatBox(box: BlockLevel, ctx: LayoutContext) {
   const cctx: LayoutContext = {...ctx, bfc: undefined};
   box.fillAreas();
@@ -1070,8 +1048,8 @@ export function layoutFloatBox(box: BlockLevel, ctx: LayoutContext) {
   let inlineSize = box.getDefiniteOuterInlineSize();
 
   if (inlineSize === undefined) {
-    const minContent = box.contribution('min-content');
-    const maxContent = box.contribution('max-content');
+    const minContent = layoutContribution(box, 'min-content');
+    const maxContent = layoutContribution(box, 'max-content');
     const availableSpace = box.getContainingBlock().inlineSize;
     const marginLineLeft = box.style.getMarginLineLeft(box);
     const marginLineRight = box.style.getMarginLineRight(box);
@@ -1423,32 +1401,6 @@ export class ReplacedBox extends FormattingBox {
     super.propagate(parent);
     parent.bitfield |= Box.BITS.hasBreakInlineOrReplaced;
     parent.bitfield |= Box.BITS.hasFloatOrReplaced;
-  }
-
-  contribution() {
-    const marginLineLeft = this.style.getMarginLineLeft(this);
-    const marginLineRight = this.style.getMarginLineLeft(this);
-    const borderLineLeftWidth = this.style.getBorderLineLeftWidth(this);
-    const paddingLineLeft = this.style.getPaddingLineLeft(this);
-    const paddingLineRight = this.style.getPaddingLineRight(this);
-    const borderLineRightWidth = this.style.getBorderLineRightWidth(this);
-    let isize = this.style.getInlineSize(this);
-    let contribution = (marginLineLeft === 'auto' ? 0 : marginLineLeft)
-      + borderLineLeftWidth
-      + paddingLineLeft
-      + paddingLineRight
-      + borderLineRightWidth
-      + (marginLineRight === 'auto' ? 0 : marginLineRight);
-
-    if (isize === 'auto') isize = this.getIntrinsicIsize();
-
-    contribution += isize;
-
-    return contribution;
-  }
-
-  getLastBaseline() {
-    return undefined;
   }
 
   getDefiniteInnerInlineSize() {

src/layout-box.ts
@@ -591,10 +591,6 @@ export abstract class FormattingBox extends Box {
   isInlineLevel() {
     return this.style.display.outer === 'inline';
   }
-
-  abstract getLastBaseline(): number | undefined;
-
-  abstract contribution(mode: 'min-content' | 'max-content'): number;
 }
 
 export class BoxArea {

src/layout-text.ts
@@ -8,7 +8,8 @@ import {
   IfcVacancy,
   Inline,
   createInlineIterator,
-  layoutFloatBox
+  layoutFloatBox,
+  layoutContribution
 } from './layout-flow.ts';
 import LineBreak, {HardBreaker} from './text-line-break.ts';
 import {nextGraphemeBreak, previousGraphemeBreak} from './text-grapheme-break.ts';
@@ -1002,9 +1003,42 @@ function baselineStep(parent: Inline, inline: Inline) {
   return 0;
 }
 
+function getLastBaseline(block: FormattingBox) {
+  const stack = [{block, offset: 0}];
+
+  while (stack.length) {
+    const {block, offset} = stack.pop()!;
+
+    if (block.isReplacedBox()) {
+      return undefined;
+    } else if (block.isBlockContainer()) {
+      if (block.isBlockContainerOfInlines()) {
+        const [ifc] = block.children;
+        const linebox = ifc.paragraph.lineboxes.at(-1);
+        if (linebox) return offset + linebox.blockOffset + linebox.ascender;
+      }
+
+      if (block.isBlockContainerOfBlocks()) {
+        const parentOffset = offset;
+
+        for (const child of block.children) {
+          if (child.isBlockContainer()) {
+            const offset = parentOffset
+              + child.getBorderArea().blockStart
+              + child.style.getBorderBlockStartWidth(child);
+              + child.style.getPaddingBlockStart(child);
+
+            stack.push({block: child, offset});
+          }
+        }
+      }
+    }
+  }
+}
+
 export function inlineFormattingBoxMetrics(box: FormattingBox) {
   const {blockStart: marginBlockStart, blockEnd: marginBlockEnd} = box.getMarginsAutoIsZero();
-  const baseline = box.style.overflow === 'hidden' ? undefined : box.getLastBaseline();
+  const baseline = box.style.overflow === 'hidden' ? undefined : getLastBaseline(box);
   let ascender, descender;
 
   if (baseline !== undefined) {
@@ -2129,7 +2163,7 @@ export class Paragraph {
       if (mark.inlinePost) width.addInk(mark.inlinePost.getInlineSideSize('post'));
 
       if (mark.box) {
-        width.addInk(mark.box.contribution(mode));
+        width.addInk(layoutContribution(mark.box, mode));
         // floats don't have breaks before/after them
         if (mode === 'min-content') {
           contribution = Math.max(contribution, width.trimmed());

commit 8ace86b4c66a94300af152e7883489f6d2c81e1f

wrong intrinsics of an box with inequal margin sizes
bug introduced in c440e11e987384dc39aa9a588b285e0ca90a61aa

src/layout-flow.ts
@@ -1137,7 +1137,7 @@ export function layoutContribution(
 ) {
   const containingBlock = box.getContainingBlock();
   const marginLineLeft = box.style.getMarginLineLeft(containingBlock);
-  const marginLineRight = box.style.getMarginLineLeft(containingBlock);
+  const marginLineRight = box.style.getMarginLineRight(containingBlock);
   const borderLineLeftWidth = box.style.getBorderLineLeftWidth(containingBlock);
   const paddingLineLeft = box.style.getPaddingLineLeft(containingBlock);
   const paddingLineRight = box.style.getPaddingLineRight(containingBlock);

test/flow.spec.js
@@ -1560,6 +1560,19 @@ describe('Flow', function () {
         expect(t.getContentArea().width).to.equal(500);
       });
 
+      it('calculates contribution from inequal margin sides correctly', function () {
+        this.layout(`
+          <div style="width: 200px;">
+            <div id="t" style="float: left; margin: 0 30px 0 10px;">
+              <div style="width: 50px; height: 10px;"></div>
+            </div>
+          </div>
+        `);
+
+        const t = this.get('#t');
+        expect(t.getContentArea().width).to.equal(50);
+      });
+
       it('considers margin, border, padding part of the intrinsic size', function () {
         this.layout(`
           <div style="width: 200px; font: 16px Arimo;">